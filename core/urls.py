e9f2319b173aa1b3105a378e2786818d498d3038
Micno <arthur.gette@gmail.com>
2024-11-10 18:59:28 +0100

    website: structure rdy

--- core/urls.py
+++ core/urls.py
@@ -8,11 +8,3 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
 ]
 
-# config/urls.py
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('core.urls')),
-]
