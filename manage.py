032b9f0a75566750b546594dd32eb66395a901e6
Tristan Siegel <130748102+Sympa1@users.noreply.github.com>
2024-11-11 15:49:44 +0000

    t

--- manage.py
+++ manage.py
@@ -16,8 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
-    # execute_from_command_line(['manage.py', 'makemigrations'])
-    
+
 
 
 if __name__ == '__main__':
